4a7d536cb2cdd0f3a9d8a0424c445849c8bdad5f
CavGame <cavalctoretgiafi9@gmail.com>
2025-01-02 12:24:28 +0100

    server_connector.py

---
+++ server_connector.py
@@ -0,0 +1,4 @@
+def connect_to_server(server_ip, nickname, password):
+    print(f"Connettendo al server {server_ip} con il nickname {nickname}...")
+    # Codice per la connessione al server (da implementare con una libreria adatta)
+    print("Connessione riuscita!")
