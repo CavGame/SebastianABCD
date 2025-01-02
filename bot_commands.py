a4f04cb96603e4206bc6960f7a711d894f2bbdcf
CavGame <cavalctoretgiafi9@gmail.com>
2025-01-02 12:25:15 +0100

    bot_commands.py

---
+++ bot_commands.py
@@ -0,0 +1,7 @@
+def move_to(position, new_position):
+    print(f"Spostamento da {position} a {new_position}...")
+    return new_position
+
+def mine_block(block_name):
+    print(f"Miniera del blocco: {block_name}...")
+    return block_name
